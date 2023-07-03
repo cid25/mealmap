527c8656030af47104bc4d6a2859298e86a361f7
Oliver Krukow <oliver.krukow@de.pwc.com>
2023-07-03 14:06:53 +0200

    Add EF Core and DbContext

--- src/Mealmap.Api/Program.cs
+++ src/Mealmap.Api/Program.cs
@@ -2,11 +2,15 @@
 using Mealmap.Repositories;
 using AutoMapper;
 using Mealmap.Api;
+using Mealmap.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IMealRepository, SqlMealRepository>();
 builder.Services.AddAutoMapper(typeof(MealMapperProfile));
+builder.Services.AddDbContext<MealmapDbContext>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("MealmapDb")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/Mealmap.Api/Repositories/MealmapDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mealmap.Api.Repositories
+{
+    public class MealmapDbContext : DbContext
+    {
+        public MealmapDbContext(DbContextOptions<MealmapDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
