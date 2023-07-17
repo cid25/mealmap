8cf4b1fe8b3989eea42afee9150a4d21e6352ded
Oliver Krukow <oliver.krukow@de.pwc.com>
2023-07-17 15:48:35 +0200

    Add UnitOfMeasurement to Model

---
+++ src/Mealmap.Model/UnitOfMeasurement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mealmap.Model
+{
+    [NotMapped]
+    public class UnitOfMeasurement
+    {
+        private static readonly IEnumerable<UnitOfMeasurement> _units;
+
+        public string Name { get; init; }
+
+        public string Symbol { get; init; }
+
+        public string? Measure { get; init; }
+
+        static UnitOfMeasurement()
+        {
+            _units = new List<UnitOfMeasurement>()
+            {
+                new UnitOfMeasurement("Gram", "g", "Mass"),
+                new UnitOfMeasurement("Kilogram", "kg", "Mass"),
+                new UnitOfMeasurement("Liter", "l", "Volume"),
+                new UnitOfMeasurement("Mililiter", "ml", "Volume"),
+                new UnitOfMeasurement("Piece", "pc", "Quantity"),
+                new UnitOfMeasurement("Bag", ""),
+                new UnitOfMeasurement("Can", ""),
+            };
+        }
+
+        private UnitOfMeasurement(string name, string symbol, string? measure = null)
+            => (Name, Symbol, Measure) = (name, symbol, measure);
+
+        public static UnitOfMeasurement FromName(string name)
+        {
+            try
+            {
+                return _units.First(x => x.Name == name);
+            }
+            catch (InvalidOperationException)
+            {
+                throw new ArgumentException("{name} must match an existing unit of measurement", name);
+            }
+        }
+    }
+}
---
+++ tests/Mealmap.Model.Tests/UnitOfMeasurementTests.cs
@@ -0,0 +1,29 @@
+﻿using Mealmap.Model;
+using FluentAssertions;
+
+namespace Mealmap.Model.Tests
+{
+    public class UnitOfMeasurementTests
+    {
+        [Fact]
+        public void FromName_WhenValidName_ReturnsUnitOfMeasurement()
+        {
+            const string validUnitName = "Kilogram";
+
+            var result = UnitOfMeasurement.FromName(validUnitName);
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<UnitOfMeasurement>();
+        }
+
+        [Fact]
+        public void FromName_WhenInvalidName_ReturnsUnitOfMeasurement()
+        {
+            const string invalidUnitName = "Kawabunga";
+
+            Action action = () => UnitOfMeasurement.FromName(invalidUnitName);
+
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
