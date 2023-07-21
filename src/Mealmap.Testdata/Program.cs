95f8c97a0cbe4032f85e2b5a7aa115de792d93b9
Oliver Krukow <oliver.krukow@de.pwc.com>
2023-07-21 09:09:06 +0200

    Minor style adjustment

--- src/Mealmap.Testdata/Program.cs
+++ src/Mealmap.Testdata/Program.cs
@@ -29,25 +29,23 @@ void InjectDishes(MealmapDbContext dbContext)
         Id = new Guid("00000000-0000-0000-0000-000000000001"),
         Description = "The fishiest burger in town.",
         Servings = 2,
-        Ingredients = new()
-            {
-                new Ingredient(4, new UnitOfMeasurement(UnitOfMeasurementCodes.Slice), "Old bread"),
-                new Ingredient(2, new UnitOfMeasurement(UnitOfMeasurementCodes.Piece), "Unidentifiable meat"),
-                new Ingredient(20, new UnitOfMeasurement(UnitOfMeasurementCodes.Mililiter), "Fishy sauce"),
-            }
+        Ingredients = new(){
+            new Ingredient(4, new UnitOfMeasurement(UnitOfMeasurementCodes.Slice), "Old bread"),
+            new Ingredient(2, new UnitOfMeasurement(UnitOfMeasurementCodes.Piece), "Unidentifiable meat"),
+            new Ingredient(20, new UnitOfMeasurement(UnitOfMeasurementCodes.Mililiter), "Fishy sauce"),
+        }
     });
     dbContext.Dishes.Add(new Dish("Sailors Surprise")
     {
         Id = new Guid("00000000-0000-0000-0000-000000000002"),
         Description = "The darkest, wettest dream of every boatsman.",
         Servings = 4,
-        Ingredients = new()
-            {
-                new Ingredient(800, new UnitOfMeasurement(UnitOfMeasurementCodes.Mililiter), "Seawater"),
-                new Ingredient(6, new UnitOfMeasurement(UnitOfMeasurementCodes.Piece), "Sea cucumber"),
-                new Ingredient(8, new UnitOfMeasurement(UnitOfMeasurementCodes.Piece), "Crab meat"),
-                new Ingredient(1, new UnitOfMeasurement(UnitOfMeasurementCodes.Pinch), "Salt"),
-            }
+        Ingredients = new(){
+            new Ingredient(800, new UnitOfMeasurement(UnitOfMeasurementCodes.Mililiter), "Seawater"),
+            new Ingredient(6, new UnitOfMeasurement(UnitOfMeasurementCodes.Piece), "Sea cucumber"),
+            new Ingredient(8, new UnitOfMeasurement(UnitOfMeasurementCodes.Piece), "Crab meat"),
+            new Ingredient(1, new UnitOfMeasurement(UnitOfMeasurementCodes.Pinch), "Salt"),
+        }
     });
 
     dbContext.SaveChanges();
@@ -58,11 +56,11 @@ void InjectMeals(MealmapDbContext dbContext)
     var today = DateTime.Today;
     DateOnly startOfWeek = DateOnly.FromDateTime(today).AddDays(-1 * (int)today.DayOfWeek + 1);
 
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 35; i++)
     {
         dbContext.Meals.Add(new Meal()
         {
-            DiningDate = startOfWeek.AddDays(i),
+            DiningDate = startOfWeek.AddDays(-14).AddDays(i),
             Dish = dbContext.Dishes.Find(Guid.Parse("00000000-0000-0000-0000-00000000000" + (i % 2 + 1).ToString())),
         });
     }
