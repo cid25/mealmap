6ab03f1eda71a967cb1d10570e60a6fa2dd4d37a
Oliver Krukow <oliver.krukow@de.pwc.com>
2023-08-02 17:57:27 +0200

    Cleanup Swagger examples for /Meals endpoints

---
+++ src/Mealmap.Api/Swagger/MealRequestExampleWithoutIdAndEtag.cs
@@ -0,0 +1,30 @@
+﻿using Mealmap.Api.DataTransferObjects;
+using Swashbuckle.AspNetCore.Filters;
+
+
+namespace Mealmap.Api.Swagger;
+
+public class MealRequestExampleWithoutIdAndEtag : IExamplesProvider<MealDTO>
+{
+    public MealDTO GetExamples()
+    {
+        return new MealDTO()
+        {
+            DiningDate = DateOnly.FromDateTime(DateTime.Now),
+            Courses = new[] {
+                new CourseDTO()
+                {
+                    Index = 1,
+                    DishId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+                    MainCourse = true
+                },
+                new CourseDTO()
+                {
+                    Index = 2,
+                    DishId = new Guid("455b6bc6-5947-402d-be8c-b2b57ca6984d"),
+                    MainCourse = false
+                },
+            }
+        };
+    }
+}
--- src/Mealmap.Api/Swagger/MealResponseExampleWithIdAndEtag.cs
+++ src/Mealmap.Api/Swagger/MealResponseExampleWithIdAndEtag.cs
@@ -4,12 +4,13 @@ using Swashbuckle.AspNetCore.Filters;
 
 namespace Mealmap.Api.Swagger;
 
-public class MealPostRequestExample : IExamplesProvider<MealDTO>
+public class MealResponseExampleWithIdAndEtag : IExamplesProvider<MealDTO>
 {
     public MealDTO GetExamples()
     {
         return new MealDTO()
         {
+            Id = Guid.NewGuid(),
             DiningDate = DateOnly.FromDateTime(DateTime.Now),
             Courses = new[] {
                 new CourseDTO()
@@ -17,7 +18,14 @@ public class MealPostRequestExample : IExamplesProvider<MealDTO>
                     Index = 1,
                     DishId = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
                     MainCourse = true
-                } }
+                },
+                new CourseDTO()
+                {
+                    Index = 2,
+                    DishId = new Guid("455b6bc6-5947-402d-be8c-b2b57ca6984d"),
+                    MainCourse = false
+                },
+            }
         };
     }
 }
