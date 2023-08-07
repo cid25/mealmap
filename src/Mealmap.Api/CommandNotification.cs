bab46941bfb13f09a01dec59933982c8833f6091
Oliver Krukow <oliver.krukow@de.pwc.com>
2023-08-07 15:39:55 +0200

    Add CommandNotification

---
+++ src/Mealmap.Api/CommandNotification.cs
@@ -0,0 +1,29 @@
+﻿namespace Mealmap.Api;
+
+public class CommandNotification<TResponse>
+{
+    public List<CommandError> Errors { get; } = new();
+
+    public bool Success
+    {
+        get => !Errors.Any();
+    }
+
+    public TResponse? Result { get; set; }
+}
+
+public class CommandError
+{
+    public CommandErrorCodes ErrorCode { get; }
+
+    public string Message { get; } = "";
+
+    public CommandError(CommandErrorCodes errorCode, string message)
+        => (ErrorCode, Message) = (errorCode, message);
+}
+
+public enum CommandErrorCodes
+{
+    NotFound,
+    NotValid
+}
---
+++ tests/Mealmap.Api.UnitTests/CommandNotificationTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+
+namespace Mealmap.Api.UnitTests;
+
+public class CommandNotificationTests
+{
+    [Fact]
+    public void Success_False_WhenErrors()
+    {
+        CommandNotification<Object> notification = new();
+
+        notification.Errors.Add(new CommandError(CommandErrorCodes.NotFound, ""));
+
+        notification.Success.Should().BeFalse();
+    }
+}
